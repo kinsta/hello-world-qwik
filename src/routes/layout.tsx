ff8213880265069f56b02325532c8544332d78bb
Kristof Dombi <dombi.kristof@gmail.com>
2024-01-03 16:36:32 +0100

    chore(kinsta): update repository

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -3,7 +3,7 @@ import { component$, Slot } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <>
-      <main>
+      <main class="max-w-[932px] pb-8 pt-16 px-10">
         <Slot />
       </main>
     </>
